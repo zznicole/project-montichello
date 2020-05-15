548c07f5091445704023f2ecd06b970d01ee84c5
Nicole Zheng <zznicole@hotmail.com>
2020-05-15 23:30:57 +0200

    add gallery html add contect map api

--- src/script.js
+++ src/script.js
@@ -1,31 +1,30 @@
 /*  header  */
-
+// nav link smooth scroll has been done in css line 4-5
 $('.slickSliderContainer').slick({
   dots: true,
   autoplay: true,
-  arrows: false
+  arrows: false,
 });
 
+/*  news  */
 $('.newsSlickSlider').slick({
   dots: true,
+  infinite: true,
+  slidesToScroll: 5,
+  slidesToShow: 5,
   autoplay: true,
 });
 
-// $(document).ready(function(){
-//   $('.slickSlider').slick({
-//     setting-name: setting-value
-//   });
-// });
+/*  contect  */
+let map = new google.maps.Map(document.getElementById("map"), {
+  center: {lat: 40.696220, lng: -73.984460},
+  zoom: 10,
+});
 
-// PROJECTS
+let marker = new google.maps.Marker({
+  position: {lat: 40.696220, lng: -73.984460},
+  map: map,
+  draggable: true,
+  icon: "../img/contact-marker.svg"
+});
 
-// NEWS
-
- s
-// GALLERY
-
-
-// CONTACT
-
-
-// FOOTER
